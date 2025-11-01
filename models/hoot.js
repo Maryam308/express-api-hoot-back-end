384a32a0152d2a5a4ce594079221fa2c33bca235
Maryam308 <mariam601me@gmail.com>
2025-11-01 14:51:45 +0300

    done crud

---
+++ models/hoot.js
@@ -0,0 +1,43 @@
+// models/hoot.js
+
+const mongoose = require("mongoose");
+
+// models/hoot.js
+
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+// models/hoot.js
+
+const hootSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["News", "Sports", "Games", "Movies", "Music", "Television"],
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    comments: [commentSchema], // add here
+  },
+  { timestamps: true }
+);
+
+const Hoot = mongoose.model("Hoot", hootSchema);
+// models/hoot.js
+
+module.exports = Hoot;
